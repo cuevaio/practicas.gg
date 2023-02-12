1671f4554740dcf2c08fcf7119f0d5dedbaa27c6
Anthony Cueva <hi.cuevantngmail.com>
2023-02-12 01:50:58 -0500

    description

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,14 @@
-export const revalidate =  43200
-
 import { use } from "react"
 import { getLongDate, getShortDate } from "@/utils"
 import { JobsClient } from "@/xata/clients"
+import Balancer from "react-wrap-balancer"
 
+import { siteConfig } from "@/config/site"
 import JobOffer from "@/components/JobOffer"
 import MorePages from "@/components/MorePages"
+import { Icons } from "@/components/icons"
+
+export const revalidate = 43200
 
 const getJobOffersPage = async () => {
   return await JobsClient.db.JobOffer.sort("publishedAt", "desc").getPaginated({
@@ -20,8 +23,15 @@ const LandingPage = () => {
   return (
     <div className="container mt-10">
       <h1 className="text-center mb-4 text-3xl font-extrabold leading-tight tracking-tighter sm:text-3xl md:text-5xl lg:text-6xl">
-        <span className="text-gray-400">Welcome to </span>civil.jobs
+        <span className="text-gray-400">Bienvenido a </span>
+        {siteConfig.name}
       </h1>
+      <div className="flex justify-between items-center dark:text-zinc-500">
+        <Balancer>{siteConfig.description}</Balancer>
+        <a target="_blank" rel="noreferrer" href={siteConfig.links.github}>
+          <Icons.gitHub className="w-5 h-5 mr-2 fill-current" />
+        </a>
+      </div>
       <div className="grid grid-cols-1 gap-8 sm:grid-cols-2 lg:grid-cols-3 mb-8 mt-12">
         {records?.map((jobOffer) => (
           <JobOffer
--- config/site.ts
+++ config/site.ts
@@ -5,18 +5,20 @@ interface SiteConfig {
     facebook: string
     instagram: string
     twitter: string
+    github: string
   }
   url: string
 }
 
 export const siteConfig: SiteConfig = {
-  name: "Civil.jobs",
+  name: "practicas.gg",
   description:
-    "Filtramos las Ofertas de empleo para Practicantes de Ingeniería Civil (60% de probabilidad de acierto)",
+    "Buscamos optimizar tu experiencia de búsqueda de empleo en Computrabajo. Por ello, hemos implementado un filtro especial para ofertas de prácticas en la rama de Ingeniería Civil. Estamos en la fase beta y agradecemos cualquier comentario o sugerencia que tengas para mejorar el sitio. Si eres desarrollador, ¡eres más que bienvenido a contribuir al código fuente en GitHub! Juntos, podemos hacer la búsqueda de empleo más eficiente y efectiva.",
   links: {
     facebook: "https://www.facebook.com/cuevantn",
     instagram: "https://www.instagram.com/cuevantn",
     twitter: "https://twitter.com/cuevantn",
+    github: "https://github.com/cuevantn/practicas.gg",
   },
   url: "https://civil.jobs",
 }
