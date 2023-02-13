0bdbc88b18b5b3025e97f40bb1da246dc6cc0943
Anthony Cueva <hi.cuevantngmail.com>
2023-02-13 12:49:02 -0500

    added salary element

--- components/JobOffer.tsx
+++ components/JobOffer.tsx
@@ -20,6 +20,7 @@ const JobOfferCardContent = ({
   location,
   publishedAt,
   body,
+  salary,
 }) => (
   <article className="dark:text-zinc-500 cursor-pointer rounded-lg border dark:border-zinc-800 hover:dark:border-zinc-700 p-4 pb-12 relative h-full text-left">
     <h2 className="dark:text-zinc-300 font-bold">{title}</h2>
@@ -28,7 +29,12 @@ const JobOfferCardContent = ({
       <span className="text-sm font-bold text-right">{location}</span>
     </div>
     <p className="whitespace-pre-line">{body.slice(0, 120)}...</p>
-    <span className="font-bold text-sm absolute bottom-2 right-2">
+    {salary && (
+      <span className="font-bold text-transparent text-sm bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600 absolute bottom-4 left-4">
+        S/ {salary}
+      </span>
+    )}
+    <span className="font-bold text-sm absolute bottom-4 right-4">
       {publishedAt.short}
     </span>
   </article>
@@ -62,7 +68,7 @@ const JobOfferDialogContent = ({
       {publishedAt.long}
     </span>
     <DialogFooter>
-      <AlertFiltrado id={id}  />
+      <AlertFiltrado id={id} />
       <Button type="button" size="sm">
         <a
           href={`https://pe.computrabajo.com/trabajo-de-practicas-ingenieria-civil#${id}`}
